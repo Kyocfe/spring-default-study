83124d6badeb6ff947ae4000c9d8aba2df87da36
Kyocfe <kyocfe@naver.com>
2022-06-07 14:18:23 +0900

    develop user jpa controller and repository

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -3,10 +3,14 @@ package com.example.demo.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,14 +20,18 @@ import java.util.Date;
 //@JsonIgnoreProperties(value={"password", "ssn"})
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "user를 위한 모델")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
     private String name;
     @Past
     private Date joinDate;
-//    @JsonIgnore
+    //    @JsonIgnore
     private String password;
-//    @JsonIgnore
+    //    @JsonIgnore
     private String ssn;
 }
